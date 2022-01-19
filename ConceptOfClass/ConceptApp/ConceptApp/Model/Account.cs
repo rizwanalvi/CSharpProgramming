797d5e3bd79a8d581512774be19cd64e31ad9809
rizwanalvi <rizwan@aptechsite.net>
2022-01-19 20:41:15 +0500

    Polymorphism implementation

---
+++ ConceptOfClass/ConceptApp/ConceptApp/Model/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptApp.Model
+{
+  public  class Account
+    {
+        public int AccountNumber { get; set; }
+        public String AccountTitle { get; set; }
+        public double AccountBalance { get; set; }
+        public void WithDraw(double amount) {
+            AccountBalance += AccountBalance - amount;
+        }
+        public void WithDraw(float amount)
+        {
+            AccountBalance += AccountBalance - amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            AccountBalance += AccountBalance + amount;
+        }
+        public void Deposit(float amount)
+        {
+            AccountBalance += AccountBalance + amount;
+        }
+
+        public virtual string AccountSummary()
+        {
+            String _Slip = String.Format("Account Number : {0}\nAccount Title : {1}\nAccount Balance :{2}", this.AccountNumber, this.AccountTitle, this.AccountBalance);
+
+            return _Slip;
+
+        }
+    }
+}
---
+++ ConceptOfClass/ConceptApp/ConceptApp/Model/SavingAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptApp.Model
+{
+    class SavingAccount :Account
+    {
+        private String _AccountType = "Saving Account";
+        public override string AccountSummary()
+        {
+            String _Slip = String.Format("Account Type : {0}\nAccount Number : {1}\nAccount Title : {2}\nAccount Balance :{3}",this._AccountType, this.AccountNumber, this.AccountTitle, this.AccountBalance);
+            return _Slip;
+        }
+    }
+}
