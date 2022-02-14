d9438cae9a3a343d961cc392e2399d38506c3c87
rizwanalvi <rizwan@aptechsite.net>
2022-02-14 19:48:55 +0500

    GridView Editin

---
+++ WebAppGridView/WebAppGridView/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebAppGridView.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">
+                <Columns>
+                    <asp:TemplateField HeaderText="PRO ID">
+                        <EditItemTemplate>
+                           <asp:Label ID="LabelX" runat="server" Text='<%# Bind("ID") %>'></asp:Label>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="Label2" runat="server" Text='<%# Bind("ID") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="MOBILE NAME">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TextBox1" runat="server" Text='<%# Bind("MNAME") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="Label1" runat="server" Text='<%# Bind("MNAME") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:CommandField ShowEditButton="True" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebAppGridView/WebAppGridView/Default.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebAppGridView
+{
+    public partial class Default : System.Web.UI.Page
+    {
+      
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+
+            }
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            LoadData();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            LoadData();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+           GridViewRow _dRow = (GridViewRow) GridView1.Rows[e.RowIndex];
+            //TextBox _txtName = (TextBox) _dRow.FindControl("TextBox1");
+            TextBox _txtName =_dRow.FindControl("TextBox1") as TextBox;
+            Label _lblId = _dRow.FindControl("LabelX") as Label;
+            try
+            {
+                SqlConnection _SqlConn = new SqlConnection(@"Data Source=FACULTY-218;Initial Catalog=MobileDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                _SqlConn.Open();
+                SqlCommand _cmd = new SqlCommand("UPDATE Mobiles SET MNAME=@MNAME WHERE ID =@ID", _SqlConn);
+                _cmd.Parameters.AddWithValue("@ID", _lblId.Text);
+                _cmd.Parameters.AddWithValue("@MNAME", _txtName.Text);
+                _cmd.ExecuteNonQuery();
+                _SqlConn.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            GridView1.EditIndex = -1;
+            LoadData();
+        }
+        private void UpdateData() {
+
+
+        
+}
+        private void LoadData()
+        {
+
+
+            try
+            {
+                SqlConnection _SqlConn = new SqlConnection(@"Data Source=FACULTY-218;Initial Catalog=MobileDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                _SqlConn.Open();
+                SqlCommand _cmd = new SqlCommand("SELECT * FROM Mobiles", _SqlConn);
+                SqlDataReader _dReader = _cmd.ExecuteReader();
+                GridView1.DataSource = _dReader;
+                GridView1.DataBind();
+                _SqlConn.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
