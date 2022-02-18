e12c248b9609b9180a1c3f85fbf7dc1cdfd495fb
rizwanalvi <rizwan@aptechsite.net>
2022-02-18 19:46:27 +0500

    Insert & Show File in Database
    Insert & Show File in Database

---
+++ WebAppImageUpload/WebAppImageUpload/WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebAppImageUpload.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Show" />
+            <br />
+            <asp:Image ID="Image1" runat="server" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="ID" HeaderText="ID" />
+                    <asp:TemplateField HeaderText="POSTER">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TextBox1" runat="server" Text='<%# Eval("IMGPATH") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Image ID="Image1" runat="server" ImageUrl='<%# "data:image/jpeg;base64," + Convert.ToBase64String((byte[])Eval("IMGPATH")) %>' />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebAppImageUpload/WebAppImageUpload/WebForm1.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebAppImageUpload
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+               
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection _sqlConn = new SqlConnection(@"Data Source=FACULTY-218;Initial Catalog=MobileDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            _sqlConn.Open();
+            SqlCommand _cmd = new SqlCommand("INSERT INTO POSTER VALUES(@IMG)",_sqlConn);
+            _cmd.Parameters.AddWithValue("@IMG", FileUpload1.FileBytes);
+            _cmd.ExecuteNonQuery();
+            _sqlConn.Close();
+            LoadData();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //Image1.ImageUrl = "data:image/jpeg;base64," + Convert.ToBase64String(FileUpload1.FileBytes);
+            SqlConnection _sqlConn = new SqlConnection(@"Data Source=FACULTY-218;Initial Catalog=MobileDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            _sqlConn.Open();
+            SqlCommand _cmd = new SqlCommand("SELECT * FROM POSTER", _sqlConn);
+           SqlDataReader _dReader =  _cmd.ExecuteReader();
+            while (_dReader.Read())
+            {
+                Image1.ImageUrl = "data:image/jpeg;base64," + Convert.ToBase64String((byte[]) _dReader["IMGPATH"]);
+            }
+        }
+
+        private void LoadData() {
+            SqlConnection _sqlConn = new SqlConnection(@"Data Source=FACULTY-218;Initial Catalog=MobileDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            _sqlConn.Open();
+            SqlCommand _cmd = new SqlCommand("SELECT * FROM POSTER", _sqlConn);
+            SqlDataReader _dReader = _cmd.ExecuteReader();
+            GridView1.DataSource = _dReader;
+            GridView1.DataBind();
+
+        }
+    }
+}
